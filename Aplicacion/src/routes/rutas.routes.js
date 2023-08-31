3aaa597e97a2aa95e8e3aa16acc0f9f0aa051cf8
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2023-08-31 13:48:24 -0500

    Delete Aplicacion directory

--- Aplicacion/src/routes/rutas.routes.js
+++
@@ -1,9 +0,0 @@
-const exp = require("express");
-const router = exp.Router();
-const controladorwassap = require('../controllers/whatsapp.controller');
-
-router
-  .get('/', controladorwassap.verificaciontoken)
-  .post('/', controladorwassap.recibirMensaje);
-
-module.exports = router;
